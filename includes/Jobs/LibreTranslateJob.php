32fd4e42976ffa0f4ca819e92eabf8dc40eca7c7
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-07 16:00:21 -0700

    Add TranslateTitle to job

--- includes/Jobs/LibreTranslateJob.php
+++ includes/Jobs/LibreTranslateJob.php
@@ -3,29 +3,37 @@
 namespace Miraheze\LibreTranslate\Jobs;
 
 use Job;
+use MediaWiki\Config\Config;
+use MediaWiki\Config\ConfigFactory;
+use Miraheze\LibreTranslate\ConfigNames;
 use Miraheze\LibreTranslate\Services\LibreTranslateUtils;
 
 class LibreTranslateJob extends Job {
 
 	public const JOB_NAME = 'LibreTranslateJob';
 
+	private Config $config;
 	private LibreTranslateUtils $libreTranslateUtils;
 
 	private string $cacheKey;
 	private string $content;
 	private string $subpage;
+	private string $titleText;
 
 	public function __construct(
 		array $params,
+		ConfigFactory $configFactory,
 		LibreTranslateUtils $libreTranslateUtils
 	) {
 		parent::__construct( self::JOB_NAME, $params );
 
+		$this->config = $configFactory->makeConfig( 'LibreTranslate' );
 		$this->libreTranslateUtils = $libreTranslateUtils;
 
 		$this->cacheKey = $params['cachekey'];
 		$this->content = $params['content'];
 		$this->subpage = $params['subpage'];
+		$this->titleText = $params['titletext'];
 	}
 
 	public function run(): bool {
@@ -40,6 +48,19 @@ class LibreTranslateJob extends Job {
 
 		// Store cache if enabled
 		$this->libreTranslateUtils->storeCache( $this->cacheKey, $translatedText );
+
+		if ( $this->config->get( ConfigNames::TranslateTitle ) ) {
+			$titleCacheKey = $this->cacheKey . '-title';
+			$titleText = $this->libreTranslateUtils->getCache( $titleCacheKey );
+			if ( !$titleText ) {
+				$titleText = $this->libreTranslateUtils->callTranslation(
+					$this->titleText, $this->subpage
+				);
+
+				$this->libreTranslateUtils->storeCache( $titleCacheKey, $titleText );
+			}
+		}
+
 		$this->libreTranslateUtils->deleteCache( $this->cacheKey . '-progress' );
 
 		return true;
