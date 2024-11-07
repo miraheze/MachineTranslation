33dfd8f9264cc07d6195777c36a45ef64285dac6
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-06 17:03:56 -0700

    Add phan

---
+++ .phan/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['minimum_target_php_version'] = '8.0';
+
+$cfg['suppress_issue_types'] = [
+	'PhanAccessMethodInternal',
+];
+
+return $cfg;
