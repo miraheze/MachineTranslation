36e44224004a8064f5dacbcec41e44c68596a1a8
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-06 18:52:02 -0700

    Add ConfigNames

---
+++ includes/ConfigNames.php
@@ -0,0 +1,23 @@
+<?php
+
+// phpcs:disable Generic.NamingConventions.UpperCaseConstantName.ClassConstantNotUpperCase
+namespace Miraheze\SubTranslate;
+
+/**
+ * A class containing constants representing the names of configuration variables,
+ * to protect against typos.
+ */
+class ConfigNames {
+
+	public const Caching = 'SubTranslateCaching';
+
+	public const CachingTime = 'SubTranslateCachingTime';
+
+	public const LibreTranslateUrl = 'SubTranslateLibreTranslateUrl';
+
+	public const RobotPolicy = 'SubTranslateRobotPolicy';
+
+	public const SuppressLanguageCaption = 'SubTranslateSuppressLanguageCaption';
+
+	public const Timeout = 'SubTranslateTimeout';
+}
