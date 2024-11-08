7e0c9e5ba50acfc9b74972679649c62cd0baee01
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-08 16:46:14 -0700

    Add

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -2,10 +2,24 @@
 
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\MediaWikiServices;
+use Miraheze\MachineTranslation\Services\LanguageUtils;
 use Miraheze\MachineTranslation\Services\MachineTranslationUtils;
 
 return [
-	'MachineTranslationUtils' => static function ( MediaWikiServices $services ): MachineTranslationUtils {
+	'MachineTranslationLanguageUtils' => static function (
+		MediaWikiServices $services
+	): LanguageUtils {
+		return new LanguageUtils(
+			$services->getHttpRequestFactory(),
+			new ServiceOptions(
+				LanguageUtils::CONSTRUCTOR_OPTIONS,
+				$services->getConfigFactory()->makeConfig( 'MachineTranslation' )
+			)
+		);
+	},
+	'MachineTranslationUtils' => static function (
+		MediaWikiServices $services
+	): MachineTranslationUtils {
 		return new MachineTranslationUtils(
 			$services->getHttpRequestFactory(),
 			$services->getObjectCacheFactory(),
