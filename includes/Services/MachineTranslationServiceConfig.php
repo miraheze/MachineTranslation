80771dc3965ce615de73d03887e54c19a33f6917
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-10 23:48:29 -0700

    Fix

--- includes/Services/MachineTranslationServiceConfig.php
+++ includes/Services/MachineTranslationServiceConfig.php
@@ -8,41 +8,42 @@ use Miraheze\MachineTranslation\ConfigNames;
 
 class MachineTranslationServiceConfig {
 
-public const CONSTRUCTOR_OPTIONS = [
+	public const CONSTRUCTOR_OPTIONS = [
 		ConfigNames::ServiceConfig,
 	];
 
-private array $serviceConfig;
+	private array $serviceConfig;
 
-public function __construct( ServiceOptions $options ) {
-	$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
-	$this->serviceConfig = $options->get( ConfigNames::ServiceConfig );
-}
-
-public function getType(): string {
-	if ( !array_key_exists( $this->serviceConfig['type'] ?? '', $this->getRequired() ) ) {
-		throw new ConfigException( 'Unsupported machine translation service configured.' );
+	public function __construct( ServiceOptions $options ) {
+		$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
+		$this->serviceConfig = $options->get( ConfigNames::ServiceConfig );
 	}
 
-	return $this->serviceConfig['type'];
-}
+	public function getType(): string {
+		if ( !array_key_exists( $this->serviceConfig['type'] ?? '', $this->getRequired() ) ) {
+			throw new ConfigException( 'Unsupported machine translation service configured.' );
+		}
 
-public function getUrl(): string {
-	if ( in_array( 'url', $this->getRequired()[$this->getType()] ) ) {
-		throw new ConfigException( 'URL is required for the configured type.' );
+		return $this->serviceConfig['type'];
 	}
 
-	return $this->serviceConfig['url'];
-}
+	public function getUrl(): string {
+		if ( in_array( 'url', $this->getRequired()[$this->getType()] ) ) {
+			throw new ConfigException( 'URL is required for the configured type.' );
+		}
 
-public function getApiKey(): string {
-	if ( in_array( 'apikey', $this->getRequired()[$this->getType()] ) ) {
-		throw new ConfigException( 'API key is required for the configured type.' );
+		return $this->serviceConfig['url'];
 	}
 
-	return $this->serviceConfig['apikey'];
-}
+	public function getApiKey(): string {
+		if ( in_array( 'apikey', $this->getRequired()[$this->getType()] ) ) {
+			throw new ConfigException( 'API key is required for the configured type.' );
+		}
 
-private function getRequired(): array {
-	return MachineTranslationUtils::REQUIRED_SERVICE_OPTIONS;
+		return $this->serviceConfig['apikey'];
+	}
+
+	private function getRequired(): array {
+		return MachineTranslationUtils::REQUIRED_SERVICE_OPTIONS;
+	}
 }
